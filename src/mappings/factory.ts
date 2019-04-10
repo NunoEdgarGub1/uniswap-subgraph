a425fdc85a9233d028ff35150f3d387c45ff729a
Jannis <jannis@thegraph.com>
2019-04-10 15:57:39 +0200

    WIP: Simplify subgraph drastically to test dynamic data sources

--- src/mappings/factory.ts
+++ src/mappings/factory.ts
@@ -1,47 +1,28 @@
-import {BigInt} from '@graphprotocol/graph-ts'
-import {
-  NewExchange
-} from '../types/Factory/Factory'
+import { BigInt } from "@graphprotocol/graph-ts";
 
-import {
-  Directory,
-  Exchange,
-} from '../types/schema'
+import { NewExchange } from "../../generated/Factory/Factory";
+import { Exchange as ExchangeTemplate } from "../../generated/Factory/templates";
+import { Directory, Exchange } from "../../generated/schema";
 
 export function handleNewExchange(event: NewExchange): void {
-  let id = event.params.exchange.toHex()
-  let exchange = new Exchange(id)
-  exchange.tokenAddress = event.params.token
-  exchange.factoryID = '1'
-  exchange.ethLiquidity = BigInt.fromI32(0)
-  exchange.ethDecimals = 18
-  exchange.tokenLiquidity = BigInt.fromI32(0)
-  exchange.startTime = event.block.timestamp
-  exchange.tradeVolume = BigInt.fromI32(0)
-  exchange.tradeCount = 0
-  exchange.totalUniToken = BigInt.fromI32(0)
-  exchange.lastTradePrice = BigInt.fromI32(0)
-  exchange.priceChange = BigInt.fromI32(0)
-  // exchange.priceChangePercent = BigInt.fromI32(0) - TODO - add this back in V2
-  exchange.price = BigInt.fromI32(0)
-  exchange.lastTradeErc20Qty = BigInt.fromI32(0)
-  exchange.lastTradeEthQty = BigInt.fromI32(0)
-  exchange.weightedAvgPrice = BigInt.fromI32(0)
-  exchange.totalValue = BigInt.fromI32(0)
-
-  // hardcoded, but technically could change with upgrades to uniswap protocol
-  exchange.version = 1
-  exchange.fee = "0.3%"
-
-  exchange.save()
-
-  let factory = Directory.load("1")
-  if (factory == null){
-    factory = new Directory("1")
-    factory.exchangeCount = 0
+  // Create the uniswap directory on demand
+  let directory = Directory.load("uniswap");
+  if (directory == null) {
+    directory = new Directory("uniswap");
+    directory.exchangeCount = 0;
   }
-  factory.exchangeCount = factory.exchangeCount + 1
+  directory.exchangeCount += 1;
+  directory.save();
 
-  factory.save()
+  // Create the exchange entity
+  let id = event.params.exchange.toHex();
+  let exchange = new Exchange(id);
+  exchange.directory = "uniswap";
+  exchange.tokenAddress = event.params.token;
+  exchange.ethLiquidity = BigInt.fromI32(0);
+  exchange.tokenLiquidity = BigInt.fromI32(0);
+  exchange.save();
+
+  // Start indexing the exchange
+  ExchangeTemplate.create(event.params.exchange);
 }
-
